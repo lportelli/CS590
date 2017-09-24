318e38d70da9443107a6510a8436d2acb13c2646
lportelli <32228271+lportelli@users.noreply.github.com>
2017-09-24 15:39:24 -0400

    finished main

--- RNP.js
+++ RNP.js
@@ -5,107 +5,132 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 var presedence = {'(':4,')':4,'POW':3,'*':2,'/':2,'%':2,'+':1,'-':1 };
-var operators = ['P','O','W','*','/','%','+','-'];
+var operators = ['P','*','/','%','+','-'];
 var digits = ['0','1','2','3','4','5','6','7','8','9','.',' '];
 
+var evaluatePostFix = function(postfixQ){
+   debug = true;
+   var eval = [];
+   var t,i;
+   var temp1, temp2, answer;
+   while (postfixQ.length != 0){
+     t = postfixQ.shift();
+     if (debug) console.log(t);
+     if (!isNaN(t)){
+       if (debug) console.log('push to eval stack');
+       eval.push(t);
+     }else if (operators.indexOf(t)>=0){
+       temp1 = ''
+       i = ''
+       while (i != ' '){
+         i = eval.pop()
+         temp1 = "".concat(i,temp1);
+         console.log(temp1,i)
+       }
+       temp1 = parseInt(temp1);
+       temp2 = ''
+       i = ''
+       while (i != ' '){
+         i = eval.pop()
+         temp2 = "".concat(i,temp2);
+         console.log(temp2,i)
+       }
+       temp2 = parseInt(temp2);
+
+       switch(t){
+         case 'POW': answer = Math.pow(temp1,temp2); break;
+         case '*': answer = temp1*temp2; break;
+         case '/': answer = temp1/temp2; break;
+         case '%': answer = temp1%temp2; break;
+         case '+': answer = temp1+temp2; break;
+         case '-': answer = temp1-temp2; break;
+       }
+       if (debug) console.log(temp1,temp2,answer);
+       eval.push(answer);
+     }
+     if (debug) console.log(eval);
+     if (debug) console.log(postfixQ);
+   }
+   console.log('Answer: ', eval[0]);
+   return eval[0];
+
+}
+
+
 var convertInfix= function(infixQ){
-  //PEMDAS
+  var debug = false
    var opS =[];
    var postfixQ =[];
    var t;
    while (infixQ.length != 0){
      t = infixQ.shift();
-     console.log(t)
+     if (debug) console.log(t)
      if (digits.indexOf(t)>-1){
-         console.log('push number to post');
+         if (debug) console.log('push number to post');
          postfixQ.push(t);
      }else if (opS.length === 0 || t=='('){
-         console.log('push operator to stack');
+         if (debug) console.log('push operator to stack');
          opS.push(t);
       }else if (t ==')'){
         while (opS[opS.length-1] != '('){
-          console.log('push top operator to post');
+          if (debug) console.log('push top operator to post');
           postfixQ.push(opS.pop());
         }
         opS.pop()
       }else {
-        console.log('t presidence',presedence[t]);
-        console.log('top op presidence',presedence[opS[opS.length-1]]);
+        if (debug) console.log('t presidence',presedence[t]);
+        if (debug) console.log('top op presidence',presedence[opS[opS.length-1]]);
         while(opS.length != 0 && opS[opS.length-1] != '(' && presedence[t]<presedence[opS[opS.length-1]]){
           console.log('push top operator to post');
          postfixQ.push(opS.pop());
         }
-       console.log('push next operator to stack');
+       if (debug) console.log('push next operator to stack');
        opS.push(t);
       }
-     console.log('operator stack',opS);
-     console.log('postfix queue',postfixQ);
+     if (debug) console.log('operator stack',opS);
+     if (debug) console.log('postfix queue',postfixQ);
    }
    while(opS.length != 0){
      postfixQ.push(opS.pop());
-     console.log('operator stack',opS);
-     console.log('postfix queue',postfixQ);
+     if (debug) console.log('operator stack',opS);
+     if (debug) console.log('postfix queue',postfixQ);
    }
-   console.log('final postfix expression',postfixQ.join(''));
+   console.log('Postfix:',postfixQ.join(''));
+   return postfixQ;
+};
 
+var parseInput = function(rawInfix){
+  var infixQ= [];
+  for(var i=0;i<rawInfix.length;i++){
+    if (rawInfix[i]=='P'){
+      infixQ.push(rawInfix.slice(i,i+3))
+      i += 2
+    }
+    else if (isNaN(rawInfix[i]) && operators.indexOf(rawInfix[i])>=0){
+      infixQ.push(' ',rawInfix[i]);
+    }else if (digits.indexOf(rawInfix[i])>=0 || rawInfix[i]=='(' || rawInfix[i]==')') {
+      infixQ.push(rawInfix[i]);
+    }else{
+      console.log('Unexpected character "',rawInfix[i],'" not added to infix expression');
+    }
+  }
+  console.log('Input: ',infixQ.join(''));
+  return infixQ;
 };
 
 var main =function(){
-  rl.question('Enter an infix expression or "q" to exit the program:',(rawInfix)=> {
+  rl.question('Enter an infix expression or "quit" to exit the program:',(rawInfix)=> {
     if (rawInfix == 'q'){
-      console.log('Exiting');
+      console.log('exiting');
       rl.close();
       return;
     }else{
-      var infixQ= [];
-      for(var i=0;i<rawInfix.length;i++){
-        if (isNaN(rawInfix[i]) && operators.indexOf(rawInfix[i])>=0){
-          infixQ.push(' ',rawInfix[i]);
-        }else if (digits.indexOf(rawInfix[i])>=0 || rawInfix[i]=='(' || rawInfix[i]==')') {
-          infixQ.push(rawInfix[i]);
-        }else{
-          console.log('Unexpected character "',rawInfix[i],'" not added to infix expression')
-        }
-      }
-
+      infixQ = parseInput(rawInfix);
       postfixQ = convertInfix(infixQ);
+      value = evaluatePostFix(postfixQ);
       main();
     }
-      // evaluatePostFix(postfixQ)
   });
 }
-main();
-// rl.close();
 
-//
-//
-// var evaluatePostFixEx(){
-//     Stack<int> eval;
-//     Queue<char> postQ;
-//     char t;
-//     int topNum, nextNum, answer;
-//
-//     while(postQ is not empty){
-//         t = postQ.Front();
-//         postQ.DeQueue();
-//         if (t is a number token)
-//             eval.Push(t)
-//         else{
-//             topNum = eval.Top();
-//             eval.Pop();
-//             nextNum = eval.Top();
-//             eval.Pop
-//         }
-//         switch(t) {
-//             case '+': answer = nextNum + topNum; break;
-//             case '-': answer = nextNum-topNum; break;
-//             case '*': answer = nextNum * topNum; break;
-//             case '/': answer = nextNum/topNum; break;
-//
-//         }
-//
-//         eval.Push(answer)
-//     }
-//
-//     return eval.Top()
-// }
+main();
